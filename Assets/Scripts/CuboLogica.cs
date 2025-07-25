4e8fab426572484edc6d85db0b3f6bd63cf028fc
Mario201019 <mruizizquierdo2010@gmail.com>
2025-07-25 17:33:34 +0200

    Primer Script

---
+++ Assets/Scripts/CuboLogica.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CuboLogica : MonoBehaviour
+{
+    //Esto es dos numero entero
+    int edadMario;
+    int edadDaniela;
+
+    //Float es un decimal
+    float alturaMario;
+    double alturaDaniela;
+
+    char a;
+    string nombreMario;
+
+    void Start()
+    {
+        edadMario = 1;
+        edadDaniela = 2;
+        Debug.Log(edadMario / edadDaniela);
+        
+    }
+
+    void Update()
+    {
+    }
+
+
+}
